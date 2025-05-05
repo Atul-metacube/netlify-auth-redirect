248a15c24ea1077f7b24f44a101f9b8265664358
Atul-metacube <atul.khandelwal.metacube@beamsuntory.com>
2025-05-05 11:31:45 +0530

    added new function

--- functions/auth-redirect.js
+++ functions/auth-redirect.js
@@ -10,28 +10,28 @@ exports.handler = async (event, context) => {
     };
   }
 
-  const cookieHeader = [
-    cookie.serialize('user_id', user_id, {
-      httpOnly: false,
-      secure: true,
-      sameSite: 'None',
-      path: '/',
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-    }),
-    cookie.serialize('email', email, {
-      httpOnly: false,
-      secure: true,
-      sameSite: 'None',
-      path: '/',
-      maxAge: 60 * 60 * 24 * 7,
-    }),
-  ];
-
   return {
     statusCode: 302,
     headers: {
-      'Set-Cookie': cookieHeader,
       Location: decodeURIComponent(redirect),
     },
+    multiValueHeaders: {
+      'Set-Cookie': [
+        cookie.serialize('user_id', user_id, {
+          httpOnly: false,
+          secure: true,
+          sameSite: 'None',
+          path: '/',
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+        }),
+        cookie.serialize('email', email, {
+          httpOnly: false,
+          secure: true,
+          sameSite: 'None',
+          path: '/',
+          maxAge: 60 * 60 * 24 * 7,
+        }),
+      ]
+    }
   };
 };
