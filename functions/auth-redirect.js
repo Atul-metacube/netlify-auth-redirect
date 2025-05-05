8b544e27fae8c977c1972c08195096c145c6b462
Atul-metacube <atul.khandelwal.metacube@beamsuntory.com>
2025-05-05 11:21:35 +0530

    Initial Netlify function

---
+++ functions/auth-redirect.js
@@ -0,0 +1,37 @@
+const cookie = require("cookie");
+
+exports.handler = async (event, context) => {
+  const { user_id, email, redirect } = event.queryStringParameters;
+
+  if (!user_id || !email || !redirect) {
+    return {
+      statusCode: 400,
+      body: 'Missing parameters',
+    };
+  }
+
+  const cookieHeader = [
+    cookie.serialize('user_id', user_id, {
+      httpOnly: false,
+      secure: true,
+      sameSite: 'None',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    }),
+    cookie.serialize('email', email, {
+      httpOnly: false,
+      secure: true,
+      sameSite: 'None',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 7,
+    }),
+  ];
+
+  return {
+    statusCode: 302,
+    headers: {
+      'Set-Cookie': cookieHeader,
+      Location: decodeURIComponent(redirect),
+    },
+  };
+};
